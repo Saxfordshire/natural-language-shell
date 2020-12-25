65a14a1f4bed41d37ece9c9aca1a4ce65d35ddc3
Saxfordshire <66988024+Saxfordshire@users.noreply.github.com>
2020-12-24 18:53:24 -0800

    Removed unneeded ", done." strings

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,7 +2,7 @@
 printf "%s\n" "Welcome to NLS! The installation will begin soon."
 rm -rf $HOME/"Natural Language Shell"
 printf "%s\n" "Verifying dependencies..."
-printf "%s" "autoinstall: checking Git installation"
+printf "%s" "autoinstall: verifying Git installation"
 if ! command -v git &> /dev/null; then
     printf "%s\n" ", done."
     printf "%s" "autointall: installing Git"
@@ -87,20 +87,17 @@ if ! command -v git &> /dev/null; then
 else
     printf "%s\n" ", done."
 fi
-printf "%s" "autoinstall: checking Go installation"
+printf "%s" "autoinstall: verifying Go installation"
 if ! command -v go &> /dev/null; then
     printf "%s\n" ", done."
-    printf "%s\n" "autointall: installing Go..."
+    printf "%s\n" "autoinstall: installing Go..."
     if [[ "$OSTYPE" == "darwin"* ]]; then
         if command -v brew &> /dev/null; then
             brew install go
-            printf "%s\n" ", done."
         elif ! command -v brew &> /dev/null; then
             /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
             brew install go
-            printf "%s\n" ", done."
         else
-            printf "%s\n" ", error!"
             printf "%s\n" "autoinstall: unable to install Go for Mac. Please follow the instructions at https://git-scm.com/download/mac"
         fi
     elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
@@ -108,76 +105,54 @@ if ! command -v go &> /dev/null; then
         # distro-specific pacage managers after to avoid potential conflicts
         if command -v snap &> /dev/null; then
             sudo snap install go --classic
-            printf "%s\n" ", done."
         elif command -v apk &> /dev/null; then 
             apk add go
-            printf "%s\n" ", done."
         elif command -v apt-get &> /dev/null; then
             sudo apt-get install go
-            printf "%s\n" ", done."
         elif command -v brew &> /dev/null; then
             brew install go
-            printf "%s\n" ", done."
         elif command -v dnf &> /dev/null; then
             sudo dnf install go
-            printf "%s\n" ", done."
         elif command -v emerge &> /dev/null; then
             sudo emerge --ask --verbose dev-vcs/git
-            printf "%s\n" ", done."
         elif command -v nix-env &> /dev/null; then
             sudo nix-env -i go
-            printf "%s\n" ", done."
         elif command -v pacman &> /dev/null; then
             sudo pacman -S go
-            printf "%s\n" ", done."
         elif command -v urpmi &> /dev/null; then
             sudo urpmi go
-            printf "%s\n" ", done."
         elif command -v yum &> /dev/null; then
             sudo yum install go
-            printf "%s\n" ", done."
         elif command -v zypper &> /dev/null; then
             sudo zypper install go
-            printf "%s\n" ", done."
         else
-            printf "%s\n" ", error!"
             printf "%s\n" "autoinstall: unable to install Git for Linux. Please follow the instructions at https://git-scm.com/download/linux"
         fi
     elif [[ "$OSTYPE" == "FreeBSD"* ]]; then
         if command -v zypper &> /dev/null; then
             sudo zypper install go
-            printf "%s\n" ", done."
         else
-            printf "%s\n" ", error!"
             printf "%s\n" "autoinstall: unable to install Git for FreeBSD. Please follow the instructions at https://git-scm.com/download/linux"
         fi
     elif [[ "$OSTYPE" == "solaris"* ]]; then
         if command -v pkgutil &> /dev/null; then
             sudo pkgutil -i go
-            printf "%s\n" ", done."
         elif command -v pkg &> /dev/null; then
             sudo pkg install developer/versioning/go
-            printf "%s\n" ", done."
         else
-            printf "%s\n" ", error!"
             printf "%s\n" "autoinstall: unable to install Git for Solaris. Please follow the instructions at https://git-scm.com/download/linux"
         fi
     elif [[ "$OSTYPE" == "openbsd"* ]]; then
         if command -v pkg_add &> /dev/null; then
             sudo pkg_add go
-            printf "%s\n" ", done."
         else
-            printf "%s\n" ", error!"
             printf "%s\n" "autoinstall: unable to install Git for OpenBSD. Please follow the instructions at https://git-scm.com/download/linux"
         fi
     else
-        printf "%s\n" ", error!"
         printf "%s\n" "autoinstall: unable to install Git. Please follow the instructions at https://git-scm.com/download"
     fi
-else
-    printf "%s\n" ", done."
 fi
-printf "%s" "autoinstall: checking Python 3 installation"
+printf "%s" "autoinstall: verifying Python 3 installation"
 if ! command -v python3 &> /dev/null
 then
     printf "%s\n" "autoinstall: Python 3 could not be found, it isn't required"
